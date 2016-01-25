0b8af4c23706bf8662836cc9314825a07e631a10
Brian Oldfield <brian.oldfield@socrata.com>
2016-01-24 22:48:06 -0800

    Pull data from S3 instead of local filesystem

--- queryApplication/build.sbt
+++ queryApplication/build.sbt
@@ -5,11 +5,20 @@ version := "1.0"
 scalaVersion := "2.10.3"
 
 libraryDependencies ++= Seq(
-  "com.databricks"   %% "spark-csv"  % "1.3.0",
-  "org.apache.spark" %% "spark-sql"  % "1.6.0" % "provided",
-  "org.apache.spark" %% "spark-core"  % "1.6.0" % "provided",
-  "spark.jobserver"  %% "job-server-api" % "0.6.1" % "provided",
-  "spark.jobserver"  %% "job-server-extras" % "0.6.1" % "provided"
+  "com.databricks"   %% "spark-csv"         % "1.3.0",
+  "org.apache.spark" %% "spark-sql"         % "1.6.0" % "provided",
+  "org.apache.spark" %% "spark-core"        % "1.6.0" % "provided",
+  "spark.jobserver"  %% "job-server-api"    % "0.6.1" % "provided",
+  "spark.jobserver"  %% "job-server-extras" % "0.6.1" % "provided",
+  "com.amazonaws"    %  "aws-java-sdk-core" % "1.10.49",
+  "com.amazonaws"    %  "aws-java-sdk-s3"   % "1.10.49"
 )
 
 resolvers += "Job Server Bintray" at "https://dl.bintray.com/spark-jobserver/maven"
+
+assemblyMergeStrategy in assembly := {
+  case PathList("about.html") => MergeStrategy.rename
+  case x =>
+    val oldStrategy = (assemblyMergeStrategy in assembly).value
+    oldStrategy(x)
+}
--- queryApplication/src/main/scala/QueryApplication.scala
+++ queryApplication/src/main/scala/QueryApplication.scala
@@ -1,32 +1,84 @@
 import com.typesafe.config.Config
+
+import java.io.InputStream
+import scala.io.Source
+import org.apache.spark._
+import org.apache.spark.SparkContext._
 import org.apache.spark.sql.SQLContext
-import spark.jobserver.{SparkJobInvalid, SparkJobValid, SparkJobValidation, SparkSqlJob}
+import spark.jobserver.{SparkJobInvalid, SparkJobValid, SparkJobValidation, SparkJob}
 
-object QueryApplication extends SparkSqlJob {
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.types.{StructType,StructField,StringType}
 
-  def runJob(sc: SQLContext, jobConfig: Config): Any = {
-    val df = sc
-      .read
-      .format("com.databricks.spark.csv")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load(jobConfig.getString("path"))
+import com.amazonaws.services.s3._
+import com.amazonaws.services.s3.model._
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable._
+
+object QueryApplication extends SparkJob {
+  def s3 = new AmazonS3Client(new EnvironmentVariableCredentialsProvider())
+
+  def listKeysInBucket(bucket: String, prefix: String): ObjectListing = {
+    val request = new ListObjectsRequest()
+    request.setBucketName(bucket)
+    request.setPrefix(prefix)
+    request.setMaxKeys(100) // need a good way to deal with this
+    s3.listObjects(request)
+  }
+
+  def runJob(sc: SparkContext, jobConfig: Config): Any = {
+    val schemaString = jobConfig.getString("schema")
+    val bucket = jobConfig.getString("bucket")
+    val prefix = jobConfig.getString("prefix")
+    val keys = listKeysInBucket(bucket, prefix)
+
+    val rdd = sc.parallelize(keys.getObjectSummaries.map(_.getKey).toList)
+                .flatMap { key =>
+                   println(key)
+                   Source.fromInputStream(s3.getObject(bucket, key).getObjectContent: InputStream).getLines
+                }
+
+    val sqc = new SQLContext(sc)
+
+    val schemaParts = schemaString.split(",")
+    val schema = StructType(
+      schemaParts.map(fieldName => StructField(fieldName.trim, StringType, true))
+    )
+    val row = rdd.map(_.split(','))
+                 // This is an attempt, albeit ugly to sidestep the array length issue described below, no joy tho
+                 .map(r => {
+                   val rLength = r.length
+                   var rPrime = r
+                   for(i <- rLength to schemaParts.length) { rPrime = rPrime :+ "" }
+                   rPrime
+                 })
+                 .map(Row.fromSeq(_)) // --> creating the row with this method _appears_ to create rows correctly but leads to index out of bounds errors on the collect step
+                 //.map(r => Row(r(0), r(1))) --> creating the row using this method leads to success but inflexible w/r to schema length
+    row.foreach(r => {
+      if (r.length < 22) {
+        println(r)
+        println(r.length)
+      }
+    })
+    val df = sqc.createDataFrame(row, schema)
     df.registerTempTable(jobConfig.getString("tablename"))
-    val result = sc.sql(jobConfig.getString("query"))
+    val result = sqc.sql(jobConfig.getString("query"))
     val columns = result.columns
     val rows = result.collect()
     Map(
       "names" -> columns,
       "values" -> rows.map(_.toSeq.map({ thing => // this is bad
         if (thing == null) "null" else thing.toString
-      }))
+       }))
     )
   }
 
-  def validate(sc: SQLContext, config: Config): SparkJobValidation =
-    if(config.hasPath("query") && config.hasPath("tablename") && config.hasPath("path"))
+  def validate(sc: SparkContext, config: Config): SparkJobValidation =
+    if(config.hasPath("schema") && config.hasPath("query") && config.hasPath("tablename") && config.hasPath("bucket") && config.hasPath("prefix"))
       SparkJobValid
     else
-      SparkJobInvalid("needs keys query, tablename, and path")
-
+      SparkJobInvalid("needs query, tablename, schema, bucket and prefix")
 }
